7ef8485b6782690d45c1cc0db476bf5a9d9553f0
KinnonZ <83826176@qq.com>
2020-04-13 12:25:13 +0800

    项链问题另一种解法

--- leetcode/necklace/Necklace.ts
+++ leetcode/necklace/Necklace.ts
@@ -2,7 +2,7 @@
  * @Author: Kinnon.Z
  * @Date: 2020-04-13 10:01:43
  * @Last Modified by: Kinnon.Z
- * @Last Modified time: 2020-04-13 10:51:06
+ * @Last Modified time: 2020-04-13 12:24:34
  */
 export default class Necklace {
     private readonly ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -43,6 +43,7 @@ export default class Necklace {
     // ? ===================================================================================
     // ? public interfaces
     // ? ===================================================================================
+    //! 第一种解法： 暴力循环,每次切割一段检测是否满足条件
     public output() {
         const necklace = this._genNecklace();
         const diamands = this._specifiyDimands();
@@ -59,7 +60,7 @@ export default class Necklace {
             find = false;
             cutnum = diamands.length;
             while (cutnum < necklace.length - i) {
-                sliced = necklace.slice(i, cutnum);
+                sliced = necklace.slice(i, cutnum + i);
                 if (diamands.every((each) => sliced.indexOf(each) >= 0)) {
                     find = true;
                     result.push({
@@ -90,6 +91,72 @@ export default class Necklace {
         let shortest = sorted.shift();
         console.log(`found the shortest segment ${shortest.sequece}, from ${shortest.start}`);
     }
+
+    //! 第二种解法：可以预测，最终的满足条件的切割段必然以关键字开始，以另一个关键字结束
+    //! 所以可以先找到所有关键字所在的位置作为锚点，以锚点之间来切割检测段
+    public output2() {
+        const necklace = this._genNecklace();
+        const diamands = this._specifiyDimands();
+
+        console.log(`necklace: ${necklace}`);
+        console.log(`dimands: ${diamands}`);
+
+        let anchors = diamands.map((each) => {
+            let mapped = necklace
+                .map((each, idx) => [each, idx])
+                .filter((e) => e[0] == each)
+                .map((e) => e[1]);
+            return mapped;
+        });
+        if (anchors.some((each) => each.length == 0)) {
+            //! 目标串中找不到某些关键字
+            return console.log("no matched segment");
+        }
+        let anchorPos: number[] = anchors
+            .reduce((pre: number[], cur: number[]) => {
+                return [...pre, ...cur];
+            }, [])
+            .sort();
+        let result: { start: number; sequece: string[]; seqLen: number }[] = [];
+
+        let startLetter: string;
+        let endLetter: string;
+        let sliced: string[];
+        for (let i = 0; i < anchorPos.length - 1; ++i) {
+            startLetter = necklace[anchorPos[i]];
+            for (let k = i + 1; k < anchorPos.length; ++k) {
+                endLetter = necklace[anchorPos[k]];
+                if (startLetter == endLetter) {
+                    continue;
+                }
+                sliced = necklace.slice(anchorPos[i], anchorPos[k] + 1);
+                if (diamands.every((each) => sliced.indexOf(each) >= 0)) {
+                    //! 找到一段
+                    result.push({
+                        start: anchorPos[i],
+                        seqLen: anchorPos[k] - anchorPos[i],
+                        sequece: sliced,
+                    });
+                }
+            }
+        }
+        if (result.length == 0) {
+            return console.log("no matched segment");
+        }
+        result.forEach((each) => {
+            console.log(
+                `matched segment ${each.sequece}  from ${each.start} with length ${each.seqLen}`
+            );
+        });
+
+        console.log("\n&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n");
+        let sorted = result.sort((a, b) => a.seqLen - b.seqLen);
+        let shortest = sorted.shift();
+        console.log(
+            `found the shortest segment ${shortest.sequece}  from ${shortest.start} with length ${shortest.seqLen}`
+        );
+    }
 }
 
-new Necklace().output();
+// new Necklace().output();
+new Necklace().output2();
